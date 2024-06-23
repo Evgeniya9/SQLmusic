fed8ca3a56612fa66165a0e2e75a5c8d23d4c071
Evgeniya9 <157239233+Evgeniya9@users.noreply.github.com>
2024-06-23 15:09:44 +0300

    Add files via upload

--- Music.sql
+++ Music.sql
@@ -9,43 +9,40 @@ create table if not exists artist (
 );
 
 create table if not exists genre_artist (
-	id_genre int,
-	id_artist int,
+	primary key (id_genre, id_artist),
 	foreign key (id_genre) references genre(id_genre),
 	foreign key (id_artist) references artist(id_artist)
 );
 
 create table if not exists album (
 	id_album int primary key,
-	id_artist int primary key,
 	name_album varchar(60) not null,
-	name_track varchar(60) not null
+	time_album time not null,
+	year_album date not null
 );
 
 create table if not exists artist_album (
-	id_artist int primary key,
-	id_album int primary key,
+	primary key (id_artist,id_album),
 	foreign key (id_artist) references artist(id_artist),
 	foreign key (id_album) references album(id_album)
 );
 
 create table if not exists track (
 	id_track int primary key,
+	id_album int primary key,
 	name_track varchar(60) not null,
-	id_album int,
+	time_track time not null,
 	foreign key (id_album) references album(id_album)
 );
 
-create table if not exists collection (
+create table if not exists collection(
 	id_collection int primary key,
-	name_album varchar(60) not null,
-	name_track varchar(60) not null,
-	year_issue date not null
+	year_issue date not null,
+	time_collection time not null
 );
 
-create table if not exists collection_track (
-	id_collection int primary key,
-	id_track int primary key,
+create table if not exists collection_track(
+	primary key (id_collection,id_track),
 	foreign key (id_collection) references collection(id_collection),
 	foreign key (id_track) references track(id_track)
 );
